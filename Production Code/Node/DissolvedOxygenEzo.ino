4e0d8651c86058d3268510df2d062ac6c60af463
ZacharyMcCann <65356378+ZacharyMcCann@users.noreply.github.com>
2020-07-17 13:26:44 -0400

    Created DissolvedOxygenEzo Class

---
+++ Production Code/Node/DissolvedOxygenEzo.ino
@@ -0,0 +1,21 @@
+#include "DissolvedOxygenEzo.h"
+
+void DissolvedOxygenEzo::sendCompensation(String temperature, String salinity) {
+  String tempCommand;
+  tempCommand.reserve("T,".length() + temperature.length());
+  tempCommand += "T,";
+  tempCommand += temperature;
+
+  String salinityCommand;
+  salinityCommand.reserve("S,".length() + salinity.length());
+  salinityCommand += "S,";
+  salinityCommand += salinity;
+  
+  Wire.beginTransmission(address);
+  Wire.write(tempCommand.c_str());
+  
+  delay(1000);
+  
+  Wire.write(salinityCommand.c_str());
+  Wire.endTransmission();
+}
