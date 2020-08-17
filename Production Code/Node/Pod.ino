e2e5ab7b0dd3ed07776cebcb96fe3c62afbd9219
SamPatriquin <49535194+SamPatriquin@users.noreply.github.com>
2020-08-17 13:51:01 -0400

    Radio commented out

--- Production Code/Node/Pod.ino
+++ Production Code/Node/Pod.ino
@@ -33,26 +33,26 @@ void Pod::enable(){
 
 void Pod::cycle(){
   String temperature = tempEzo.takeReading();
-  sendRadioMessage(nodeId, String(tempEzo.getAddress()), temperature);
+  //sendRadioMessage(nodeId, String(tempEzo.getAddress()), temperature);
   tempEzo.toSleep();
   delay(1000);
   
   String orp = orpEzo.takeReading();
-  sendRadioMessage(nodeId, String(orpEzo.getAddress()), orp);
+  //sendRadioMessage(nodeId, String(orpEzo.getAddress()), orp);
   orpEzo.toSleep();
   delay(1000);
   
   condEzo.sendCompensation(temperature);
   delay(1000);
   String conductivity = condEzo.takeReading();
-  sendRadioMessage(nodeId, String(condEzo.getAddress()), conductivity);
+  //sendRadioMessage(nodeId, String(condEzo.getAddress()), conductivity);
   condEzo.toSleep();
   delay(1000);
   
   doEzo.sendCompensation(temperature, conductivity);
   delay(1000);
   String dissolvedOxygen = doEzo.takeReading();
-  sendRadioMessage(nodeId, String(doEzo.getAddress()), dissolvedOxygen);
+  //sendRadioMessage(nodeId, String(doEzo.getAddress()), dissolvedOxygen);
   doEzo.toSleep();
 }
 
